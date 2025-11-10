83f962302580e4a8ddcf184eecb0f54db6e93b11
jingyao <LHR3221312632@gmail.com>
2025-11-10 16:44:47 +0800

    feat: 初始化Spring Boot项目结构

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/JingyaoAiCodeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingyaoAiCodeAssistantApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JingyaoAiCodeAssistantApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/jingyao/jingyaoaicodeassistant/JingyaoAiCodeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingyaoAiCodeAssistantApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
