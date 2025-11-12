748ece75766c429b4bae9c7659b88fc9e4c2778d
jingyao <LHR3221312632@gmail.com>
2025-11-12 16:47:12 +0800

    feat: 添加MyBatis Mapper扫描配置

--- src/main/java/com/jingyao/jingyaoaicodeassistant/JingyaoAiCodeAssistantApplication.java
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/JingyaoAiCodeAssistantApplication.java
@@ -1,11 +1,13 @@
 package com.jingyao.jingyaoaicodeassistant;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.jingyao.jingyaoaicodeassistant.mapper")
 public class JingyaoAiCodeAssistantApplication {
 	
 	public static void main(String[] args) {
