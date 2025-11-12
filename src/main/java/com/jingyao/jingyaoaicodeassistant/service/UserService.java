0f39a5da49e78c1a40f9f12605b39db7114e9a68
jingyao <LHR3221312632@gmail.com>
2025-11-12 16:46:28 +0800

    feat: 添加用户服务接口

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/service/UserService.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant.service;
+
+import com.mybatisflex.core.service.IService;
+import com.jingyao.jingyaoaicodeassistant.model.entity.User;
+
+/**
+ * 用户 服务层。
+ *
+ * @author <a href="https://github.com/jingyao152">JINGYAO</a>
+ */
+public interface UserService extends IService<User> {
+
+}
