a51b2baf1a252b46bed56a5b910663bbdec2fc6b
jingyao <LHR3221312632@gmail.com>
2025-11-12 16:46:23 +0800

    feat: 添加用户映射接口UserMapper

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.jingyao.jingyaoaicodeassistant.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/jingyao152">JINGYAO</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
