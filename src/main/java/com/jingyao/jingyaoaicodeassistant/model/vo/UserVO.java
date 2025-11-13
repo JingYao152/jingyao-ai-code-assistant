511b644c40d24d5af19d6bc8d9ff77ddd6d06fbd
jingyao <LHR3221312632@gmail.com>
2025-11-13 16:19:06 +0800

    feat: 添加用户视图对象UserVO

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/model/vo/UserVO.java
@@ -0,0 +1,44 @@
+package com.jingyao.jingyaoaicodeassistant.model.vo;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * 用户视图对象，用于传输用户相关数据。
+ */
+@Data
+public class UserVO implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+	/**
+	 * id
+	 */
+	private Long id;
+	/**
+	 * 账号
+	 */
+	private String userAccount;
+	/**
+	 * 用户昵称
+	 */
+	private String userName;
+	/**
+	 * 用户头像
+	 */
+	private String userAvatar;
+	/**
+	 * 用户简介
+	 */
+	private String userProfile;
+	/**
+	 * 用户角色：user/admin
+	 */
+	private String userRole;
+	/**
+	 * 创建时间
+	 */
+	private LocalDateTime createTime;
+}
