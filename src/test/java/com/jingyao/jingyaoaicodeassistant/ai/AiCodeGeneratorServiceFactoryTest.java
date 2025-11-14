02569a9e2e8e145fc3d4b4e2903f7d954de05da2
jingyao <LHR3221312632@gmail.com>
2025-11-14 11:04:43 +0800

    test: 更新测试类返回类型为具体结果对象

--- src/test/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorServiceFactoryTest.java
+++ src/test/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorServiceFactoryTest.java
@@ -1,5 +1,7 @@
 package com.jingyao.jingyaoaicodeassistant.ai;
 
+import com.jingyao.jingyaoaicodeassistant.ai.model.HtmlCodeResult;
+import com.jingyao.jingyaoaicodeassistant.ai.model.MultiFileCodeResult;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -7,19 +9,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AiCodeGeneratorServiceTest {
-	// TODO:大模型输出过多内容时会出现超时问题，需要优化
 	@Resource
 	private AiCodeGeneratorService aiCodeGeneratorService;
 	
 	@Test
 	void generateHtmlCode() {
-		String result = aiCodeGeneratorService.generateHtmlCode("做个工作记录小工具");
+		HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个工作记录小工具");
 		Assertions.assertNotNull(result);
 	}
 	
 	@Test
 	void generateMultiFileCode() {
-		String multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个留言板小工具");
+		MultiFileCodeResult multiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个留言板小工具");
 		Assertions.assertNotNull(multiFileCode);
 	}
 }
