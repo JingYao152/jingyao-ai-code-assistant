3998f0357beb2b742e36d43ec635e5abdb34178c
jingyao <LHR3221312632@gmail.com>
2025-11-14 11:34:10 +0800

    feat: 添加流式代码生成接口

--- src/main/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorService.java
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.jingyao.jingyaoaicodeassistant.ai;
 import com.jingyao.jingyaoaicodeassistant.ai.model.HtmlCodeResult;
 import com.jingyao.jingyaoaicodeassistant.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
 	
@@ -25,5 +26,23 @@ public interface AiCodeGeneratorService {
 	 */
 	@SystemMessage(fromResource = "prompt/multi-file-system-prompt.txt")
 	MultiFileCodeResult generateMultiFileCode(String userMessage);
+	
+	/**
+	 * 生成 HTML 代码（流式）
+	 *
+	 * @param userMessage 用户消息
+	 * @return 生成的代码结果
+	 */
+	@SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+	Flux<String> generateHtmlCodeStream(String userMessage);
+	
+	/**
+	 * 生成多文件代码（流式）
+	 *
+	 * @param userMessage 用户消息
+	 * @return 生成的代码结果
+	 */
+	@SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+	Flux<String> generateMultiFileCodeStream(String userMessage);
 	
 }
