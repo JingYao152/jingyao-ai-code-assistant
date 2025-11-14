0ac0c54ec4eed2a26d5b866f5bfc903c12b6a2ba
jingyao <LHR3221312632@gmail.com>
2025-11-14 11:02:52 +0800

    feat: 添加HTML代码结果类的描述注解

--- src/main/java/com/jingyao/jingyaoaicodeassistant/ai/model/HtmlCodeResult.java
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/ai/model/HtmlCodeResult.java
@@ -1,16 +1,20 @@
 package com.jingyao.jingyaoaicodeassistant.ai.model;
 
+import dev.langchain4j.model.output.structured.Description;
 import lombok.Data;
 
 /**
  * HTML代码结果类，用于封装HTML代码及其描述信息
  */
 @Data
+@Description("生成 HTML 代码文件的结果")
 public class HtmlCodeResult {
 	
 	// HTML代码内容
+	@Description("HTML代码")
 	private String htmlCode;
 	
 	// 描述信息
+	@Description("生成代码的描述")
 	private String description;
 }
