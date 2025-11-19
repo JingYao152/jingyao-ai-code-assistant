d6f75d2b22f59e07b2f4c7a17e847332cb068a99
jingyao <LHR3221312632@gmail.com>
2025-11-19 11:14:47 +0800

    feat: 添加代码解析器接口

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/core/parser/CodeParser.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant.core.parser;
+
+/**
+ * 代码解析器策略入口
+ */
+public interface CodeParser<T> {
+	/**
+	 * 解析代码内容
+	 * @param codeContent 代码内容
+	 * @return 解析后的结果
+	 */
+	T parseCode(String codeContent);
+}
