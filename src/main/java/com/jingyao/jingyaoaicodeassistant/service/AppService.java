0caa7195128299855b9c08b9fdb15bb2b250b313
jingyao <LHR3221312632@gmail.com>
2025-11-21 16:23:59 +0800

    feat: 添加AppService接口

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/service/AppService.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant.service;
+
+import com.mybatisflex.core.service.IService;
+import com.jingyao.jingyaoaicodeassistant.model.entity.App;
+
+/**
+ * 应用 服务层。
+ *
+ * @author <a href="https://github.com/jingyao152">JINGYAO</a>
+ */
+public interface AppService extends IService<App> {
+
+}
