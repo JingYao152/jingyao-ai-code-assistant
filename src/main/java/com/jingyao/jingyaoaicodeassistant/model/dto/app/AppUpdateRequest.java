dedcfa1323775b264c87d026b59eb8825a358a7a
jingyao <LHR3221312632@gmail.com>
2025-11-21 16:35:09 +0800

    feat: 添加应用更新请求DTO类

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/model/dto/app/AppUpdateRequest.java
@@ -0,0 +1,20 @@
+package com.jingyao.jingyaoaicodeassistant.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class AppUpdateRequest implements Serializable {
+	@Serial
+	private static final long serialVersionUID = 1L;
+	/**
+	 * id
+	 */
+	private Long id;
+	/**
+	 * 应用名称
+	 */
+	private String appName;
+}
