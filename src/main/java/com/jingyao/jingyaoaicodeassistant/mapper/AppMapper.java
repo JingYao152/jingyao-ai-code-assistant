0af10774f9bab8b515372c0a24b7fdc543ed9e25
jingyao <LHR3221312632@gmail.com>
2025-11-21 16:23:52 +0800

    feat: 新增AppMapper接口

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/mapper/AppMapper.java
@@ -0,0 +1,13 @@
+package com.jingyao.jingyaoaicodeassistant.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.jingyao.jingyaoaicodeassistant.model.entity.App;
+
+/**
+ * 应用 映射层。
+ *
+ * @author <a href="https://github.com/jingyao152">JINGYAO</a>
+ */
+public interface AppMapper extends BaseMapper<App> {
+
+}
