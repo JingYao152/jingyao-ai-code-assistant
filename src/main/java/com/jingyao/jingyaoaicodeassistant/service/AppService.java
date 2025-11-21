1d6247f390000b5d9b823aa08df4b1d26a3d3cef
jingyao <LHR3221312632@gmail.com>
2025-11-21 16:57:42 +0800

    feat: 添加批量获取AppVO列表方法

--- src/main/java/com/jingyao/jingyaoaicodeassistant/service/AppService.java
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/service/AppService.java
@@ -6,6 +6,8 @@ import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.jingyao.jingyaoaicodeassistant.model.entity.App;
 
+import java.util.List;
+
 /**
  * 应用 服务层。
  *
@@ -15,4 +17,6 @@ public interface AppService extends IService<App> {
 	AppVO getAppVO(App app);
 	
 	QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
+	
+	List<AppVO> getAppVOList(List<App> appList);
 }
