73ea7a5a1a7ea53b931182a9752deaf3e1e6e4c3
jingyao <LHR3221312632@gmail.com>
2025-11-26 15:24:19 +0800

    feat: 添加应用部署请求类

---
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/model/dto/app/AppDeployRequest.java
@@ -0,0 +1,18 @@
+package com.jingyao.jingyaoaicodeassistant.model.dto.app;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+public class AppDeployRequest implements Serializable {
+	
+	@Serial
+	private static final long serialVersionUID = 1L;
+	/**
+	 * 应用 id
+	 */
+	private Long appId;
+}
+
