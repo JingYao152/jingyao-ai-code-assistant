c2816092279e30de9a9d1717f14dc2cf3508cc70
jingyao <LHR3221312632@gmail.com>
2025-12-06 09:36:01 +0800

    feat: 添加Vue项目代码生成流式接口

--- src/main/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorService.java
+++ src/main/java/com/jingyao/jingyaoaicodeassistant/ai/AiCodeGeneratorService.java
@@ -2,7 +2,9 @@ package com.jingyao.jingyaoaicodeassistant.ai;
 
 import com.jingyao.jingyaoaicodeassistant.ai.model.HtmlCodeResult;
 import com.jingyao.jingyaoaicodeassistant.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
 import reactor.core.publisher.Flux;
 
 public interface AiCodeGeneratorService {
@@ -45,4 +47,6 @@ public interface AiCodeGeneratorService {
 	@SystemMessage(fromResource = "prompt/multi-file-system-prompt.txt")
 	Flux<String> generateMultiFileCodeStream(String userMessage);
 	
+	@SystemMessage(fromResource = "prompt/codegen-vue-project-system-prompt.txt")
+	Flux<String> generateVueProjectCodeStream(@MemoryId long appId, @UserMessage String userMessage);
 }
